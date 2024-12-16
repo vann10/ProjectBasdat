b73d0f577f7497e13406276e187f6787b3e8c55c
Irvan <149338975+vann10@users.noreply.github.com>
2024-12-16 16:18:16 +0700

    update evl

--- app/routes/routesEvaluasi.py
+++ app/routes/routesEvaluasi.py
@@ -52,3 +52,41 @@ def tableEvaluasi():
         return render_template('tableEvaluasi.html', table=table, total_pages=total_pages, current_page=page)
     else:
         return render_template('tableEvaluasi.html', table=None)
+
+# Create Data
+@routesEvaluasi.route('/Create/createEvaluasi', methods=['GET', 'POST'])
+def create_Evaluasi():
+    # Handle the form submission when the method is POST
+    if request.method == 'POST':
+        # properti input digunakan disini
+        evaluasi_id_evaluasi = request.form['id_evaluasi_Evaluasi']
+        evaluasi_id_siswa = request.form['id_siswa_Evaluasi']
+        evaluasi_status = request.form['status_Evaluasi']
+        evaluasi_rerata_kehadiran = request.form['rerata_kehadiran_Evaluasi']
+        evaluasi_rerata_nilai = request.form['rerata_nilai_Evaluasi']
+
+        # Get a connection to the database
+        conn = create_connection()
+        
+        # Check if the connection was successful
+        if conn:
+            cursor = conn.cursor()
+            try:
+                # Insert the new tableA into the database
+                cursor.execute('INSERT INTO Evaluasi (id_evaluasi, id_siswa, status, rerata_kehadiran, rerata_nilai) VALUES (?, ?, ?, ?, ?)', 
+                               (evaluasi_id_evaluasi,evaluasi_id_siswa, evaluasi_status, evaluasi_rerata_kehadiran, evaluasi_rerata_nilai))
+                conn.commit()  # Commit the transaction
+                
+                # Redirect to the tableA list with a success message
+                flash('TableA added successfully!', 'success')
+                return redirect(url_for('routesEvaluasi.tableEvaluasi'))
+            except Exception as e:
+                flash(f'Error: {str(e)}', 'danger')  # Flash error message
+            finally:
+                cursor.close()
+                conn.close()
+        
+        flash('Failed to connect to the database', 'danger')  # Error if connection failed
+
+    # Render the form for GET request
+    return render_template('Create/createEvaluasi.html')
